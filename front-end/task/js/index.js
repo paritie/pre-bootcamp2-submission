e54f6cca8de42d1342a0508cd18355f5bf6ef7b5
Iyadi Ol'seun <42003510+CornerstoneII@users.noreply.github.com>
2020-01-01 20:52:03 +0000

    Oluwaseun Iyadi

---
+++ front-end/task/js/index.js
@@ -0,0 +1,3 @@
+<script src="https://code.jquery.com/jquery-3.4.1.slim.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.js"></script>
