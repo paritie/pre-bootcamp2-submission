4e34e6cb6ebf73ad529331af30ae906da29cc51f
Abdulwahab Ashimi <princeadebowale3@gmail.com>
2020-01-02 11:06:26 +0100

    Abdulwahab Adebowale Ashimi

---
+++ django/my_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/data/data/com.termux/files/home/pre-bootcamp2-submission/django/my_venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
